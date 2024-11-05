83dd2afa5feffb30b727cc3bbe684dfb4f19251e
NT-000 <nicolai.thue.reier@gmail.com>
2024-11-05 11:41:36 +0100

    Update ban/filter function,  model
    Filter function now sorts out any unwanted users and autoBan them.
    
    Added some users in the model to illustrate how the function works.

--- JS/mainFeedController.js
+++ JS/mainFeedController.js
@@ -1,53 +1,80 @@
-function goToMainFeed(){
+function goToMainFeed() {
     model.app.currentPage = model.app.pages[3];
     changeView();
 }
 
-function showEm(){
+function showEm() {
     selectedUser = model.data.users[model.input.profile.selectedUser];
-    
+
+    imprisonUser();
+
     let personArray = filterMenWomen();
-    personArray = personArray.filter(user => !selectedUser.liked.includes(user.id) && !selectedUser.disliked.includes(user.id) && !model.data.adminPage.bannedUser.includes(user.id));
+    personArray = personArray.filter(user => !selectedUser.liked.includes(user.id) && !selectedUser.disliked.includes(user.id) && !model.data.adminPage.bannedUser.includes(user.id) &&
+    !user.isPrincessCat && !user.isImposter);
+
     console.log("personarray:", personArray)
-    let randomNum = Math.floor(Math.random()*personArray.length)
+    
+    if (personArray.length === 0) {
+        model.input.loginUser.logInMesssage = '';
+        model.input.loginUser.logInMesssage =`<strong>There are no users left to display in the feed.<strong>`;
+        return null;
+    }
+    let randomNum = Math.floor(Math.random() * personArray.length)
     console.log("randomNum:", randomNum)
     let foundPerson = personArray[randomNum];
+    if (foundPerson.isPrincessCat || foundPerson.isImposter && model.data.adminPage.bannedUser.includes(foundPerson.id)){
+        model.data.adminPage.bannedUser.push(foundPerson.id);
+        console.log("banned user:", foundPerson)
+        updateMainFeedView();
+    }
     console.log("foundPerson:", foundPerson);
     return foundPerson;
 }
 
+function imprisonUser(){
+    let bannedUsers = filterMenWomen().filter(user => user.isPrincessCat || user.isImposter);
+
+    for(let i = 0; i < bannedUsers.length;i++){
+        let xUser = bannedUsers[i];
+        if(!model.data.adminPage.bannedUser.includes(xUser.id)){
+            model.data.adminPage.bannedUser.push(xUser.id);
+            console.log(`banned user: ${xUser.username}`);
+        }        
+    }
+}
+
 function filterMenWomen() {
     selectedUser = model.data.users[model.input.profile.selectedUser];
-    if(selectedUser.gender === 'male'){
-    return model.data.users.filter(user => user.showMen);
+    if (selectedUser.gender === 'male') {
+        return model.data.users.filter(user => user.showMen);
     }
-    if(selectedUser.gender === 'female'){
+    if (selectedUser.gender === 'female') {
         return model.data.users.filter(user => user.showWomen);
     }
 }
 
-function like(){
-selectedUser = model.data.users[model.input.profile.selectedUser];
-selectedUser.liked.push(model.input.profile.selectedProfileUser);
-console.log("liked:", model.input.profile.selectedProfileUser);
-console.log("liked array:", model.data.users[0].liked)
-updateMainFeedView();
+function like() {
+    selectedUser = model.data.users[model.input.profile.selectedUser];
+    selectedUser.liked.push(model.input.profile.selectedProfileUser);
+    console.log("liked:", model.input.profile.selectedProfileUser);
+    console.log("liked array:", model.data.users[0].liked)
+    updateMainFeedView();
 }
 
-function dislike(){
+function dislike() {
 
-model.data.users[model.input.profile.selectedUser].disliked.push(model.input.profile.selectedProfileUser);
-console.log("disliked:", model.input.profile.selectedProfileUser);
-updateMainFeedView();   
+    model.data.users[model.input.profile.selectedUser].disliked.push(model.input.profile.selectedProfileUser);
+    console.log("disliked:", model.input.profile.selectedProfileUser);
+    updateMainFeedView();
 }
 
-function banUser(){
+function banUser() {
     model.data.adminPage.bannedUser.push(model.input.profile.selectedProfileUser);
     console.log("banned:", model.input.profile.selectedProfileUser);
     updateMainFeedView();
 }
 
-function goToFeedProfile(){
+function goToFeedProfile() {
     model.app.currentPage = model.app.pages[7];
     changeView();
 }
--- JS/model.js
+++ JS/model.js
@@ -13,7 +13,7 @@ const model = {
       "adminPage",
       "feedProfilePage"
     ],
-    currentPage: "adminPage",
+    currentPage: "userLoginPage",
   }, // app Syntax
 
 
@@ -82,12 +82,14 @@ const model = {
         gender: "male",
         age: 34,
         location: "Larvik",
-        bio: "lorem ipsum sin dalor",
-        interests: ["Ting 1", "Ting 2", "Ting 3", "Ting 4"],
+        bio: "Liker å skru, spille på slurva og kjøre sykkel",
+        interests: ["Martial Arts", "Riding Motorcycles", "Parkour",],
         interestsBox: ['Martial Arts', 'Riding Motorcycles','Surfing','The Office', 'Gaming','Netflix', 'Parkour', 'FurryBallin',],
         liked: [],
         disliked: [],
         messages: [],
+        isPrincessCat: false,
+        isImposter: false,
 
         feed: [],
         mcClub: "Hells Angels",
@@ -115,6 +117,8 @@ const model = {
         liked: [],
         disliked: [],
         messages: [],
+        isPrincessCat: false,
+        isImposter: false,
 
         feed: [],
         mcClub: "Lakselv Bandidos",
@@ -122,13 +126,43 @@ const model = {
         userImages: ["Assets/Boycats/boycat4.webp","Assets/Boycats/boycat9.webp"],
       },
 
+      {
+        //Male Users
+        id: 2,
+        username: "CatMan",
+        password: "123",
+        email: "mrcat@hotmail.com",
+        isAdmin: false,
+        owner: null,
+        showMen: true,
+        showWomen: true,
+
+        name: "Jimmy",
+
+        gender: "male",
+        age: 28,
+        location: "Indre Østfold",
+        bio: "Purr purr.",
+        interests: ["FuttyBallin'", "Parkour",],
+        interestsBox: ['Martial Arts', 'Riding Motorcycles','Surfing','The Office', 'Gaming','Netflix', 'Parkour', 'FurryBallin',],
+        liked: [],
+        disliked: [],
+        messages: [],
+        isPrincessCat: false,
+        isImposter: true,
+
+        feed: [],
+        mcClub: "None",
+        userIMG: "",
+        userImages: ["Assets/Misc/catman.png","Assets/Misc/catperson.png"],
+      },
 
 
       //Female Users
 
 
       {
-        id: 2,
+        id: 3,
         username: "Emma",
         password: "123",
         email: "emma.brown@example.com",
@@ -147,6 +181,8 @@ const model = {
         liked: [],
         disliked: [],
         messages: [],
+        isPrincessCat: false,
+        isImposter: false,
 
         feed: [],
         mcClub: "None",
@@ -154,10 +190,10 @@ const model = {
         userImages: ["Assets/Girlcats/girlcat1.webp"],
       },
       {
-        id: 3,
+        id: 4,
         username: "Anna",
         password: "123",
-        email: "olivia.williams@example.com",
+        email: "anna.williams@example.com",
         isAdmin: false,
         owner: null,
         showMen: true,
@@ -173,11 +209,69 @@ const model = {
         liked: [],
         disliked: [],
         messages: [],
+        isPrincessCat: false,
+        isImposter: false,
 
         feed: [],
         mcClub: "None",
-        userIMG: "Assets/Girlcats/girlcat2.webp",
-        userImages: ["Assets/Girlcats/girlcat2.webp"],
+        userIMG: "",
+        userImages: ["Assets/Girlcats/girlcat3.webp","Assets/Girlcats/girlcat2.webp"],
+      },
+      {
+        id: 5,
+        username: "Diana",
+        password: "123",
+        email: "diana.brown@example.com",
+        isAdmin: false,
+        owner: null,
+        showMen: true,
+        showWomen: false,
+        name: "Diana",
+
+        gender: "female",
+        age: 25,
+        location: "Buckingham Palace",
+        bio: "I have a dog-servant.",
+        interests: ["FurryBallin'", "Food"],
+        interestsBox: ['Martial Arts', 'Riding Motorcycles','Surfing','The Office', 'Gaming','Netflix', 'Parkour', 'FurryBallin',],
+        liked: [],
+        disliked: [],
+        messages: [],
+        isPrincessCat: true,
+        isImposter: false,
+
+        feed: [],
+        mcClub: "None",
+        userIMG: "",
+        userImages: ["Assets/Misc/pcatGrey.png", "Assets/Misc/pcatGrey2.png",],
+      },
+      {
+        id: 6,
+        username: "MissMargareth",
+        password: "123",
+        email: "margareth@example.com",
+        isAdmin: false,
+        owner: null,
+        showMen: true,
+        showWomen: false,
+        name: "Emma",
+
+        gender: "female",
+        age: 25,
+        location: "The Rockefeller Estate",
+        bio: "Always hold your nose up high",
+        interests: ["FurryBallin'", "Food"],
+        interestsBox: ['Martial Arts', 'Riding Motorcycles','Surfing','The Office', 'Gaming','Netflix', 'Parkour', 'FurryBallin',],
+        liked: [],
+        disliked: [],
+        messages: [],
+        isPrincessCat: false,
+        isImposter: false,
+
+        feed: [],
+        mcClub: "None",
+        userIMG: "",
+        userImages: ["Assets/Misc/pcatBrown.png","Assets/Misc/pcatGrey2.png",],
       },
       ], //Users Syntax
       
